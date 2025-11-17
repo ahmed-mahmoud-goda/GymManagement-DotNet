b7bba5be1477ff86e797160125664b2b9b6c0df2
ahmed-mahmoud-goda <ahmed-goda@hotmail.com>
2025-11-17 19:29:49 +0200

    Finished Member Service

---
+++ GymManagementBLL/Services/Interfaces/IMemberService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GymManagementBLL.ViewModels.MemberViewModels;
+
+namespace GymManagementBLL.Services.Interfaces
+{
+    public interface IMemberService
+    {
+        bool CreateMember(CreateMemberViewModel model);
+        bool UpdateMemberDetails(int memberId,MemberToUpdateViewModel model);
+        bool RemoveMember(int memberId);
+        IEnumerable<MemberViewModel> GetAllMembers();
+        MemberViewModel? GetMemberDetails(int memberId);
+        HealthRecordViewModel? GetMemberHealthRecord(int memberId);
+        MemberToUpdateViewModel? GetMemberToUpdate(int memberId);
+    }
+}
